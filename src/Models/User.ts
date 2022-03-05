8ce62d88f27f682a78da80cba542ab5ed2d14fdd
Kambaia Alberto <kambaiaalberto@gmail.com>
2022-03-05 17:57:46 +0100

    ...

--- src/Models/User.ts
+++ src/Models/User.ts
@@ -7,7 +7,7 @@ export enum CUSTOM_VALIDATION {
   }
 
 const userSchema: Schema = new Schema({
-	email: { type: String, required: true, unique: true },
+	email: { type: String, required: true },
 	firstName: { type: String, required: true },
 	lastName: { type: String, required: true },
 	phoneNumber: { type: String, required: true},
