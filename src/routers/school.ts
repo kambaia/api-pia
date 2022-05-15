6bcb330ecdc8599340146191e418473e75afb889
Kambaia Alberto <kambaiaalberto@gmail.com>
2022-05-15 06:48:47 +0100

    remove method getschool access

--- src/routers/school.ts
+++ src/routers/school.ts
@@ -4,7 +4,6 @@ import schoolController from '../controllers/schoolController ';
 export const schoolRouter = Router();
 schoolRouter.get('/api/schools', schoolController.listAllSchool);
 schoolRouter.get('/api/school/:schoolId', schoolController.listOneSchool);
-schoolRouter.get('/api/school_access/:userId', schoolController.listOneAcesss);
 schoolRouter.post('/api/school', schoolController.saveSchool);
 schoolRouter.put('/api/school/:schoolId', schoolController.updateSchool);
 schoolRouter.delete('/api/school/:schoolId', schoolController.deleteSchool);
